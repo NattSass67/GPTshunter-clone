34ba9f7e5c898ebb6f50546c2e71cf44acc3f550
NattSass67 <nattanun.sk@gmail.com>
2024-06-05 22:42:47 +0700

    first-commit

--- src/components/Submit.tsx
+++ src/components/Submit.tsx
@@ -39,7 +39,7 @@ export function SubmitSimple() {
       <div className="w-full rounded-lg bg-white p-6 md:p-10">
         <label className="mb-2 flex text-sm">* Email</label>
         <input
-          className="flex h-full w-full rounded-md px-3 py-2 text-sm text-zinc-800 ring-1 ring-inset ring-gray-200 focus:outline-none focus:ring-gray-400 dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
+          className="flex h-full w-full rounded-md mb-1 px-3 py-2 text-sm text-zinc-800 ring-1 ring-inset ring-gray-200 focus:outline-none focus:ring-gray-400 dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10"
           placeholder=""
           value={email}
           onChange={(event) => {
