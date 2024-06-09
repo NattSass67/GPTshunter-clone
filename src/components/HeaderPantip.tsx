89877045c82ac6fbdff1ec90b4ccb2fee35d60a7
NattSass67 <nattanun.sk@gmail.com>
2024-06-09 19:29:01 +0700

    fix

--- src/components/HeaderPantip.tsx
+++ src/components/HeaderPantip.tsx
@@ -76,7 +76,7 @@ function ButtonChangeLang() {
   const local = usePathname().split('/')[1]
   const router = useRouter()
   return (
-    <div className="relative z-50 flex flex-col">
+    <div className="relative z-50 flex flex-col" onBlur={()=>{setClicked(false)}}>
       <button
         className="group rounded-full bg-white/90 px-3 py-3 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur transition dark:bg-zinc-800/90 dark:ring-white/10 dark:hover:ring-white/20"
         onClick={() => {
